271755038861495109ea89c0c0b9c28adae183b6
Timur <t.fralik@aimprosoft.com>
2021-07-22 19:03:13 +0300

    Add tests for checking DynamicComparison

--- src/DynamicComparison.php
+++ src/DynamicComparison.php
@@ -10,9 +10,9 @@ class DynamicComparison
 
     private array $result = [];
 
-    private int $maxScore = 0;
+    private array $best = ['maxScore' => 0, 'name' => ''];
 
-    public function __constructor(string $seek, $array)
+    public function __construct(string $seek, $array)
     {
         $this->seek = mb_strtolower($seek);
         if (is_array($array)) {
@@ -25,34 +25,31 @@ class DynamicComparison
 
     public function getMaxScores(): int
     {
-        return $this->maxScore;
+        return $this->best['maxScore'];
     }
 
     public function getBestResult(): string
     {
-
-    }
-
-    public function getBestResults(): array
-    {
-
+        return $this->best['name'];
     }
 
     private function calculate()
     {
         for ($i = 0; $i < count($this->array); $i++) {
             $word = mb_strtolower($this->array[$i]);
-            for ($j = 0; $j < count($this->seek); $j++) {
-                for ($k = 0; $k < count($word); $k++) {
+            for ($j = 0; $j < mb_strlen($this->seek); $j++) {
+                for ($k = 0; $k < mb_strlen($word); $k++) {
                     $this->result[$word][$j][$k] = 0;
                     if ($this->seek[$j] == $word[$k]) {
                         if (($j !== 0) && ($k !== 0) && ($this->result[$word][$j-1][$k-1] !== 0)) {
                             $this->result[$word][$j][$k] = $this->result[$word][$j-1][$k-1] + 1;
-                            if ($this->maxScore < $this->result[$word][$j][$k]) {
-                                $this->maxScore = $this->result[$word][$j][$k];
-                            }
                         } else {
                             $this->result[$word][$j][$k] = 1;
+                        }
+                        if ($this->best['maxScore'] < $this->result[$word][$j][$k]) {
+                            $this->best['maxScore'] = $this->result[$word][$j][$k];
+                            $this->best['name'] = $word;
+
                         }
                     }
                 }
--- tests/DynamicComparisonTest.php
+++ tests/DynamicComparisonTest.php
@@ -12,6 +12,32 @@ class DynamicComparisonTest extends TestCase
 
         $this->assertSame(1, $compare->getMaxScores());
         $this->assertSame('a', $compare->getBestResult());
-        $this->assertSame(['a'], $compare->getBestResults());
+    }
+
+    /** @test */
+    public function it_compares_with_several()
+    {
+        $compare = new DynamicComparison('abc', ['abf', 'avc', 'sbv']);
+
+        $this->assertSame(2, $compare->getMaxScores());
+        $this->assertSame('abf', $compare->getBestResult());
+    }
+
+    /** @test */
+    public function it_compares_with_different_length()
+    {
+        $compare = new DynamicComparison('abcd', ['sdabcd', 'abcsdjki']);
+
+        $this->assertSame(4, $compare->getMaxScores());
+        $this->assertSame('sdabcd', $compare->getBestResult());
+    }
+
+    /** @test */
+    public function it_compares_with_gap()
+    {
+        $compare = new DynamicComparison('first name', ['firs', 'rst name', 'namedfasfasdf']);
+
+        $this->assertSame(8, $compare->getMaxScores());
+        $this->assertSame('rst name', $compare->getBestResult());
     }
 }
