bf1a86ad3f5793bdf3f9efb5a2006dfd31b8324b
Timur11timur <timur__timur@mail.ru>
2021-07-22 09:03:16 +0300

    Add calculate method

--- src/DynamicComparison.php
+++ src/DynamicComparison.php
@@ -6,14 +6,26 @@ class DynamicComparison
 {
     private string $seek;
 
+    private array $array;
+
+    private array $result = [];
+
+    private int $maxScore = 0;
+
     public function __constructor(string $seek, $array)
     {
-        $this->seek = $seek;
+        $this->seek = mb_strtolower($seek);
+        if (is_array($array)) {
+            $this->array = $array;
+        } else {
+            $this->array = [$array];
+        }
+        $this->calculate();
     }
 
     public function getMaxScores(): int
     {
-
+        return $this->maxScore;
     }
 
     public function getBestResult(): string
@@ -24,5 +36,27 @@ class DynamicComparison
     public function getBestResults(): array
     {
 
+    }
+
+    private function calculate()
+    {
+        for ($i = 0; $i < count($this->array); $i++) {
+            $word = mb_strtolower($this->array[$i]);
+            for ($j = 0; $j < count($this->seek); $j++) {
+                for ($k = 0; $k < count($word); $k++) {
+                    $this->result[$word][$j][$k] = 0;
+                    if ($this->seek[$j] == $word[$k]) {
+                        if (($j !== 0) && ($k !== 0) && ($this->result[$word][$j-1][$k-1] !== 0)) {
+                            $this->result[$word][$j][$k] = $this->result[$word][$j-1][$k-1] + 1;
+                            if ($this->maxScore < $this->result[$word][$j][$k]) {
+                                $this->maxScore = $this->result[$word][$j][$k];
+                            }
+                        } else {
+                            $this->result[$word][$j][$k] = 1;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
